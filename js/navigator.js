3cc3bd0832a8c930ba372e2cf765fea34ede71e5
fujunzhuo <1134476937@qq.com>
2017-01-19 15:59:26 +0800

    'RollerSkatingApp'

---
+++ js/navigator.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import TabNavigator from 'react-native-tab-navigator';
+
+class RSNavigator extends Component {
+    render() {
+        return (
+            <TabNavigator />
+        );
+    }
+}
+
+module.exports = RSNavigator;
