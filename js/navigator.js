bebe1c9cfda033a27ea6b7bffbd988359e345078
fujunzhuo <1134476937@qq.com>
2017-01-22 16:28:34 +0800

    'RollerSkatingApp'

--- js/navigator.js
+++ js/navigator.js
@@ -5,57 +5,87 @@ import {
     Image,
     Text,
     View,
+    StatusBar
 } from 'react-native';
 
 import TabNavigator from 'react-native-tab-navigator';
 
-class RSNavigator extends Component{
+class CustomBadgeView extends Component {
+    render(){
+        return(
+            <View>
+                <Text style={{fontSize:5}}></Text>
+            </View>
+        );
+    }
+}
+class RSNavigator extends Component {
+
     constructor(props){
         super(props);
         this.state = {
-            selectedTab:'index'
+            selectedTab:'home'
         }
     }
 
-    render(){
-        return(
+    render() {
 
-            <View style={styles.container} >
-                <TabNavigator tabBarStyle={{height:200}}>
-                    <TabNavigator.Item style={{height:200}}
-                        selected={this.state.selectedTab === 'index'}
-                        title="首页"
-                        badgeText={10}
-                        titleStyle={styles.tabText}
-                        selectedTitleStyle={styles.selectedTabText}
-                        renderIcon={() => <Image style={styles.icon} source={require("./img/01.jpg")} />}
-                        renderSelectedIcon={() => <Image style={styles.icon} source={require("./img/01.jpg")} />}
-                        onPress={() => this.setState({ selectedTab: '首页' })}>
-                        <Text>首页</Text>
+        return (
+            <View style={{paddingTop:65}}>
+                <TabNavigator tabBarStyle={styles.tabBarStyle} sceneStyle={styles.sceneStyle}>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'home'}
+                        title="Home"
+                        tabStyle={styles.tabStyle}
+                        renderIcon={() => <Image source={require('./images/tab/tab_myself_n.png')} style={styles.tabIcon}/>}
+                        renderSelectedIcon={() => <Image source={require('./images/tab/tab_myself_p.png')} style={styles.tabIcon}/>}
+                        badgeText="1"
+                        onPress={() => this.setState({ selectedTab: 'home' })}>
+                        <Image source={require('./images/demo/slideshow5.jpg')} style={{height:500}} />
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'profile'}
+                        title="Profile"
+                        tabStyle={styles.tabStyle}
+                        renderIcon={() => <Image source={require('./images/tab/tab_discovery_n.png')} style={styles.tabIcon}/>}
+                        renderSelectedIcon={() => <Image source={require('./images/tab/tab_discovery_p.png')} style={styles.tabIcon}/>}
+                        onPress={() => this.setState({ selectedTab: 'profile' })}>
+                        <Image source={require('./images/demo/slideshow4.jpg')} style={{height:500}} />
                     </TabNavigator.Item>
                 </TabNavigator>
-            </View>
 
+            </View>
         );
     }
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
+    tabBarStyle:{
+        height:65,
+        justifyContent:'center',
+        borderWidth:1,
+        borderColor:'blue',
+        borderStyle:'dashed',
+        backgroundColor: '#eee',
+        alignItems: 'center'
     },
-    tabText: {
-        color: "#000000",
-        fontSize: 13
+    sceneStyle:{
+        height:400,
+        alignItems:'center',
+        justifyContent:'center',
+        borderWidth:1,
+        borderColor:'red',
+        borderStyle:'dashed'
     },
-    selectedTabText: {
-        color: "#999999",
-        fontSize: 13
+    tabStyle:{
+        height:65,
+        alignItems:'center',
+        justifyContent:'center',
     },
-    icon: {
-        width: 20,
-        height: 20
+    tabIcon:{
+        width:25,
+        height:25,
     }
 });
-module.exports = ()=> <RSNavigator />;
+
+module.exports = RSNavigator;
